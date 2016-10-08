e311a88d3c38c4b61d7ace87361cacbc304551e1
Marcus A Hansen <Marcush@live.dk>
2016-10-08 21:55:27 +0200

    Added a description page for more detail on the movies

--- js/app.js
+++ js/app.js
@@ -19,9 +19,55 @@ var movieSearch = ! function() {
 
     var url = 'http://www.omdbapi.com/'; //The standard url
 
+    /**
+     * @desc This is the click event. The evet is activated when the user clicks
+     *        on the image poster for any movie
+     *
+     * @param  {Event} event Mouse click event
+     * @return {nil}         We don't return anything
+     */
+    var movieDescription = function( event ) {
+
+      /**
+       * The data we send with the ajax get request
+       * @type {Object}
+       */
+      var data = {
+        i: $(event.target).parent().attr('alt'),
+        plot: 'full',
+        r: 'json'
+      };
+
+      /**
+       * This callback function loads all of the returned json data into
+       * the correct locations, and show the description page afterwards
+       *
+       * @param  {Objec} json        The JSON object with our data
+       * @param  {String} textStatus The status of the ajax request (if it succeeded)
+       * @return {nil}               We don't retrun anything
+       */
+      var loadDescription = function(json, textStatus) {
+        if (textStatus === 'success') { //If the ajax request was successfull
+          //Here we load all of the different data points into their respective locations
+          $('.description-img img').attr('src', json.Poster);
+          $('.description-top h1').text(json.Title.toUpperCase() + ' (' + json.Year + ')');
+          $('.description-top p').text('IMDb Rating: ' + json.imdbRating);
+          $('.description-bottom p').text(json.Plot);
+          $('.description-bottom a').attr('href', 'http://www.imdb.com/title/' + json.imdbID +'/');
+          $('.movie-description').slideDown('slow'); //We show the description page
+        }
+      };
+
+      $.getJSON(url, data,loadDescription); //Start the ajax request
+    };
+
+    //The hide button on the description page
+    $('#hideDesc').click(function(event) {
+      $('.movie-description').slideUp('slow'); //We hide the description page
+    });
+
     $('form').submit(function(event) { //The form is submitted
         event.preventDefault();
-
         /**
          * @desc The data object that we send with te get request with our search values
          * @type {Object}
@@ -33,7 +79,7 @@ var movieSearch = ! function() {
         };
 
         /**
-         * @desc This function takes care of the ui when loading a movie
+         * @desc This callback function takes care of the ui when loading a movie
          * @param  {Object} json       The JSON data that we get back from our request
          * @param  {String} textStatus The status of the ajax request (if it succeeded)
          * @return {Nil}               We don't return anything
@@ -55,7 +101,7 @@ var movieSearch = ! function() {
                        * The only real logic here is the .Poster part, since we may or may not receive a poster link
                        * So we have to tjek if there is a poster or not
                        */
-                        returnHTML += '<li><div class="poster-wrap">';
+                        returnHTML += '<li><div class="poster-wrap" alt="' + serchResult.imdbID + '">';
                         if (serchResult.Poster === "N/A") {
                             returnHTML += '<i class="material-icons poster-placeholder">crop_original</i>';
                         } else {
@@ -69,12 +115,13 @@ var movieSearch = ! function() {
                 }
                 //We add the fresh HTML to the DOM, and fade it in
                 $('#movies').append(returnHTML).fadeIn(400);
+                $('#movies img').click(movieDescription);
             }
         };
 
         //This is where we start all of the code above
         $('#movies').fadeOut(400, function() { //First we fade out the movies already showing
-            $.getJSON(url, data, uiFunction); //When we are done, we try to download the new movie data
+            $.getJSON(url, data, uiFunction); //When we are done, we start the ajax request
         });
     });
 }();
